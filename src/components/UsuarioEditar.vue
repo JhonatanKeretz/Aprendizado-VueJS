97a47d4d85fb1cdfe8f158c3628538688b8ff3cb
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-03-30 22:04:07 -0300

    Add files via upload

---
+++ src/components/UsuarioEditar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="componente">
+        <h2>Alterar os Dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p>Idade do Usuário é: <strong>{{ idade }}</strong></p>
+        <button @clicl="alterarIdade">Alterar Idade</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["Idade"],
+  methods: {
+      alterarIdade(){
+          this.idade = 33
+      }
+  }
+    
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+</style>
